dd0a112d3d37f05db00b535eeed590b9bb334610
Boy Kurniawan <boykurniawan123@gmail.com>
2022-02-27 18:13:40 +0700

    up

--- application/views/user_member/klaim_reward.php
+++ application/views/user_member/klaim_reward.php
@@ -78,9 +78,16 @@ $point_saya = point_saya($id_member);
                                     ?>
                               </td>
                               <td>
-                                   <?php if ($point_saya >= $row->poin_target): ?>
-                                        <a href="app/ajukan_klaim/<?php echo $id_member ?>/<?php echo $row->id_reward ?>" class="btn btn-outline-info px-1">Pilih</a>
-                                   <?php endif ?>
+                                   <?php
+                                   $this->db->where('id_member', $id_member);
+                                   $this->db->where('id_reward', $row->id_reward);
+                                   $cek = $this->db->get('klaim_reward');
+                                   if ($cek->num_rows() > 0) {
+                                        if ($ $point_saya >= $row->poin_target): ?>
+                                             <a href="app/ajukan_klaim/<?php echo $id_member ?>/<?php echo $row->id_reward ?>" class="btn btn-outline-info px-1">Pilih</a>
+
+
+                                   <?php } endif ?>
                               </td>
                               
                          </tr>
