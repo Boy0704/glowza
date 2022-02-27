41cb7d4992d9d7575157d136156a0358e716f278
Boy Kurniawan <boykurniawan123@gmail.com>
2022-02-27 17:47:14 +0700

    up

--- application/views/produk/produk_list.php
+++ application/views/produk/produk_list.php
@@ -18,7 +18,10 @@
                               <th>Kode Produk</th>
                               <th>Nama Produk</th>
                               <th>Deskripsi</th>
-                              <th>Harga Beli</th>
+                              <?php if ($this->session->userdata('level') == 'superadmin'): ?>
+                                   <th>Harga Beli</th>
+                              <?php endif ?>
+                              
                               <th>Harga Jual</th>
                               <th>Qty</th>
                               <th>Action</th>
@@ -34,20 +37,24 @@
                               <td><?php echo $produk->kode_produk ?></td>
                               <td><?php echo $produk->nama_produk ?></td>
                               <td><?php echo $produk->deskripsi ?></td>
-                              <td><?php echo $produk->harga_beli ?></td>
+                              <?php if ($this->session->userdata('level') == 'superadmin'): ?>
+                                   <td><?php echo $produk->harga_beli ?></td>
+                              <?php endif ?>
                               <td><?php echo $produk->harga_jual ?></td>
                               <td><?php echo $produk->qty ?></td>
-                              <td style="text-align:center" width="200px">
+                              <?php if ($this->session->userdata('level') == 'superadmin'): ?>
+                                   <td style="text-align:center" width="200px">
 
-                                   <a href="produk/update/<?php echo $produk->id_produk ?>" title="Update Data"
-                                        class="btn btn-sm btn-primary"><i class="bx bx-edit me-0"></i>
-                                   </a>
-                                   <a href="produk/delete/<?php echo $produk->id_produk ?>" title="Hapus Data"
-                                        onclick="javasciprt: return confirm('Yakin akan hapus data ini ?')"
-                                        class="btn btn-sm btn-danger"><i class="bx bx-trash-alt me-0"></i>
-                                   </a>
+                                        <a href="produk/update/<?php echo $produk->id_produk ?>" title="Update Data"
+                                             class="btn btn-sm btn-primary"><i class="bx bx-edit me-0"></i>
+                                        </a>
+                                        <a href="produk/delete/<?php echo $produk->id_produk ?>" title="Hapus Data"
+                                             onclick="javasciprt: return confirm('Yakin akan hapus data ini ?')"
+                                             class="btn btn-sm btn-danger"><i class="bx bx-trash-alt me-0"></i>
+                                        </a>
 
-                              </td>
+                                   </td>
+                              <?php endif ?>
                          </tr>
                          <?php
                             $no++;
