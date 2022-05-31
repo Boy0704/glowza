5adabb5d654ec06f9dfa117aa6de9b8aeef006f8
Boy Kurniawan <boykurniawan123@gmail.com>
2022-05-31 13:23:50 +0700

    up

--- application/views/agent.php
+++ application/views/agent.php
@@ -29,7 +29,7 @@
                     <div class="flex flex-wrap justify-start">
                          <div class="">
                               <div class="brand w-40 py-10">
-                                   <img src="front_agen/logo.png" alt="">
+                                   <img src="front_agen/logo.png" style="width: 100px;" alt="">
                               </div>
                               <div class="w-8/12 lg:w-7/12 px-2 text-left">
                                    <h1 class="text-red-300  text-3xl md:text-5xl font-ramaraja">
@@ -42,11 +42,11 @@
                               </div>
                               <div id="button" class="mt-10 my-2  flex flex-wrap gap-2">
                                    <a href="https://api.whatsapp.com/send?phone=<?php echo $no_telp ?>&amp;text="
-                                        data-message="Saya ada tertarik dengan produk justmine. Apakah bisa dibantu?"
+                                        data-message="Saya ada tertarik dengan produk glowza. Apakah bisa dibantu?"
                                         class="member_mobilephone_themes_agen bg-blue-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         id="wa-buy"><span class="text-xs" id="wa-buy">Beli Sekarang</span></a>
                                    <a href="https://api.whatsapp.com/send?phone=<?php echo $no_telp ?>&amp;text="
-                                        data-message="Saya tertarik bergabung dengan justmine. Apakah bisa dibantu?"
+                                        data-message="Saya tertarik bergabung dengan glowza. Apakah bisa dibantu?"
                                         class="member_mobilephone_themes_agen bg-green-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         id="wa-join"><span class="text-xs">Gabung Menjadi Mitra</span></a>
                               </div>
@@ -321,7 +321,7 @@
                               kamu menjadi bagian dari Glowza Skincare
                          </p>
                          <a href="https://api.whatsapp.com/send?phone=<?php echo $no_telp ?>&amp;text="
-                              data-message="Saya ada tertarik dengan produk justmine. Apakah bisa dibantu?" id="wa-buy"
+                              data-message="Saya ada tertarik dengan produk glowza. Apakah bisa dibantu?" id="wa-buy"
                               class="member_mobilephone_themes_agen bg-blue-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                               Beli Sekarang
                          </a>
@@ -438,11 +438,11 @@
                </div>
                <div class="text-center">
 
-                    <button
+                    <!-- <button
                          class="bg-blue-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                          type="button" onclick="toggleModal('modal-id')">
                          Hadiah Lainnya
-                    </button>
+                    </button> -->
                </div>
                <section id="modalHadiah">
 
@@ -1200,7 +1200,7 @@
 
      <div class="whatsapp fixed bottom-5 right-5 z-50">
           <a href="https://api.whatsapp.com/send?phone=<?php echo $no_telp ?>&amp;text="
-               data-message="Saya ada tertarik dengan justmine. Apakah bisa dibantu?" id="wa-contact" target="_blank"
+               data-message="Saya ada tertarik dengan glowza. Apakah bisa dibantu?" id="wa-contact" target="_blank"
                class="wa-ask member_mobilephone_themes_agen" data-tip data-for="whatsapp">
                <svg viewBox="0 0 32 32" class="whatsapp-icon rounded-full ">
                     <path d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z"
@@ -1218,7 +1218,7 @@
                </h1>
                <div class="my-5">
                     <a href="https://api.whatsapp.com/send?phone=<?php echo $no_telp ?>&amp;text="
-                         data-message="Saya ada ada pertanyaan mengenai justmine. Apakah bisa dibantu?" id="wa-buy"
+                         data-message="Saya ada ada pertanyaan mengenai glowza. Apakah bisa dibantu?" id="wa-buy"
                          class="member_mobilephone_themes_agen  bg-blue-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                          id="wa-contact">
                          Hubungi Kami
